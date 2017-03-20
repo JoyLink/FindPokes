0f34fc0521bce920d5d65fac12d4b339898edb8e
Joy <joylinkyizhuo@gmail.com>
2017-03-19 18:56:36 -0700

    bridge headers

---
+++ FindPokes/FindPokes-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  FindPokes-Bridging-Header.h
+//  FindPokes
+//
+//  Created by Joy on 3/19/17.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+#import "GeoFire.h"
---
+++ Pods/Target Support Files/Pods-FindPokes/Pods-FindPokes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FindPokes : NSObject
+@end
+@implementation PodsDummy_Pods_FindPokes
+@end
